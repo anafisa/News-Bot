21bcc1945f38fd37b1f37093532d35db4b1c56ec
Anfisa <anfisa_lepekhina5@mail.ru>
2019-11-05 18:45:49 +0300

    add rss tool

--- parser.py
+++ parser.py
@@ -14,3 +14,15 @@ def news(category, word):
                 news.append(post.link)
     return news
 
+
+def rss_news(rss, word):
+    stemmer = SnowballStemmer("russian")
+    w = stemmer.stem(word)
+    news = []
+    feed = feedparser.parse(rss)
+    for post in feed.entries:
+        lst = list(map(stemmer.stem, (post.title + post.description).split()))
+        if w in lst:
+            news.append(f"{post.title} \n{post.description}")
+    return news
+
